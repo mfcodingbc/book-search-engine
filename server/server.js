450a7f2b2efb42ade4f28a823081a8d00045c806
Mitchell Frey <mfcodingbc@gmail.com>
2022-07-03 18:37:40 -0400

    project init npm libraries installed from copied code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+// import ApolloServer package
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
