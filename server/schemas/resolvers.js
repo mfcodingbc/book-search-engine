84f5d18c48f9e7df2449c60e679697943134cdca
Mitchell Frey <mfcodingbc@gmail.com>
2022-07-03 20:37:12 -0400

    fixed query.mutation issue (wrong location in resolvers.js)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,43 +15,43 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in');
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
     },
-    Mutation: {
-      addUser: async (parent, args) => {
-        const user = await User.create(args);
-        const token = signToken(user);
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-        return { token, user };
-      },
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-
-        if (!user) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-
-        const correctPw = await user.isCorrectPassword(password);
-
-        if (!correctPw) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-
-        const token = signToken(user);
-        return { token, user };
-      },
-      saveBook: async (parent, { bookId }, context) => {
-        if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $addToSet: { books: bookId } },
-            { new: true }
-          ).populate('savedBooks');
-
-          return updatedUser;
-        }
-
-        throw new AuthenticationError('You need to be logged in!');
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
       }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { books: bookId } },
+          { new: true }
+        ).populate('savedBooks');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
     }
   }
 };
