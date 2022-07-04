ab5f9a94ef9c5272e411352c39bbd41f0a86c754
Mitchell Frey <mfcodingbc@gmail.com>
2022-07-03 23:54:10 -0400

    more code revision...

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -9,13 +9,13 @@ import {
   CardColumns
 } from 'react-bootstrap';
 
-import { useMutation } from '@apollo/client';
+// import { useMutation } from '@apollo/client';
 
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/API';
+import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
-import { SAVE_BOOK } from '../utils/mutations';
+// import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
